3059945798cfa505eb44b30cb44f310dadef7160
李铭昕 <715557344@qq.com>
2018-02-24 20:19:20 +0800

    完善注册

--- src/common/api.js
+++ src/common/api.js
@@ -5,21 +5,21 @@ import wepy from 'wepy'
 
 var api = {
   baseUrl: 'http://api.dtm.phalcon.xin',
-  post: function (route, data) {
+  post: function (route, params) {
     let self = this
     return new Promise((resolve, reject) => {
       var token = wepy.getStorageSync('token')
       var data = {
         url: self.baseUrl + route,
-        data: data,
+        data: params,
         method: 'POST',
         success: function (response) {
           if (response.statusCode == 200) {
             data = response.data
+            console.log(data)
             if (data.success) {
               resolve(data)
             } else {
-              console.log(data)
               if (data.errorCode == 700) {
                 wepy.redirectTo({
                   url: 'pages/login'
