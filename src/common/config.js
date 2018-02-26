d933e6eb377510605eee24ef596553c750605d72
李铭昕 <715557344@qq.com>
2018-02-26 21:03:25 +0800

    修改端口号

--- src/common/config.js
+++ src/common/config.js
@@ -3,7 +3,7 @@
  */
 var config = {
   apiUrl: 'http://api.dtm.phalcon.xin',
-  wsUrl: 'ws://127.0.0.1:12000'
+  wsUrl: 'ws://127.0.0.1:12100'
   // apiUrl: 'https://dtm.phalcon.lmx0536.cn',
   // wsUrl: 'wss://dtm.phalcon.lmx0536.cn'
 }
