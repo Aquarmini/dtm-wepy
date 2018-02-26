ae210bee6a696c281fb4e3730a71265fc80cdea5
李铭昕 <715557344@qq.com>
2018-02-26 21:24:43 +0800

    增加websocket

--- src/common/websocket.js
+++ src/common/websocket.js
@@ -5,15 +5,34 @@ import wepy from 'wepy'
 import config from './config'
 
 var websocket = {
+  // 是否连接上Websocket
   open: false,
+  // 发送消息的缓冲区
+  messages: [],
+
   connect() {
+    let self = this
     wepy.connectSocket({
       url: config.wsUrl
     })
 
     wepy.onSocketOpen(function (res) {
-      console.log(res)
+      self.open = true
+      var message
+      while (message = self.messages.pop()) {
+        self.send(message)
+      }
     })
+  },
+
+  send(message) {
+    if (this.open) {
+      wepy.sendSocketMessage({
+        data: JSON.stringify(message)
+      })
+    } else {
+      this.messages.push(message)
+    }
   }
 }
 
